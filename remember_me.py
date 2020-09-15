be0b527a35c179c09793a0bceb4c40fb69970e9b
Michael Lu <yeskky@gmail.com>
2020-09-15 09:32:39 +0800

    继续重构

--- remember_me.py
+++ remember_me.py
@@ -9,15 +9,18 @@ def get_stored_username():
         return None
     else:
         return username
+def get_new_username():
+    username = input('What is your name?')
+    with open(filename, 'w') as f:
+        json.dump(username, f)
+    return username
 
 def greet_user():
     username=get_stored_username()
     if username:
         print(f"Welcome back, {username}!")
     else:
-        username = input('What is your name?')
-        with open(filename, 'w') as f:
-            json.dump(username, f)
-            print(f'We\'ll remember you when you come back, {username}!')
+        username=get_new_username()
+        print(f'We\'ll remember you when you come back, {username}!')
 
 greet_user()        
