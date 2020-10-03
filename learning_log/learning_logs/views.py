2a5cfd7cfd521b0f7adcc64668318a84e79394d7
Michael Lu <yeskky@gmail.com>
2020-10-03 17:19:53 +0800

    fix typo that caused topics not shown

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -12,7 +12,7 @@ def index(request):
 def topics(request):
     """显示所有主题"""
     topics = Topic.objects.order_by('date_added')
-    context = {'topic': topics}
+    context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
 
