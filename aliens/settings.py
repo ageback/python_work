4a923911bb8bace44934661636b4bf4b1c67bd08
Michael Lu <yeskky@gmail.com>
2020-10-06 11:38:15 +0800

    用小数存储飞船的速度值

--- aliens/settings.py
+++ aliens/settings.py
@@ -3,6 +3,9 @@ class Settings:
     def __init__(self):
         self.screen_width = 1000
         self.screen_height = 650
+
+        # 飞船设置
+        self.ship_speed = 1.5
         self.screen_x = 0
         self.screen_y = 0
         self.bg_color = (230, 230, 230)
