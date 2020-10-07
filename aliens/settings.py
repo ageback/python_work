1e51949ebf1535af01d3eff7b95a46b181262d1d
Michael Lu <yeskky@gmail.com>
2020-10-07 11:39:21 +0800

    移动外星人群

--- aliens/settings.py
+++ aliens/settings.py
@@ -14,3 +14,9 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+
+        # 外星人设置
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction 为 1 表示向右移，为 -1 表示向左移。
+        self.fleet_direction = 1
