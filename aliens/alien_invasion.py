d46a20239ac661f107e6f21f3178ea0403000028
Michael Lu <yeskky@gmail.com>
2020-10-07 09:06:53 +0800

    删除消失的子弹

--- aliens/alien_invasion.py
+++ aliens/alien_invasion.py
@@ -31,6 +31,12 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self.bullets.update()
+
+            # 删除消失的子弹
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+                        
             self._update_screen()
 
     def _fire_bullet(self):
